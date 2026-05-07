b54b1aefcd66530d32ea5a6a13c56926f36c5d09
Dikshya Pokharel <dikshya.pokharel16@gmail.com>
2026-05-07 14:36:07 +0200

    Add GitHub Pages base path and deploy script

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Wolfsburg-Investigating-Tool/',
   build: {
     rollupOptions: {
       output: {
